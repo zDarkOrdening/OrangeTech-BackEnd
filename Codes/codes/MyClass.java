d21ea1b15863971dafaa9a8554c35effd650fc49
zDarkOrdening <zdarkordening@gmail.com>
2023-03-06 15:41:37 -0300

    testing if changes are working

--- Codes/codes/MyClass.java
+++ Codes/codes/MyClass.java
@@ -5,8 +5,7 @@ public class MyClass {
         System.out.println(fullName("Leonardo", "Lima"));
     }
     public static String fullName(String firstName, String lastName) {
-//        return firstName + " " + lastName;
-        return firstName.concat(" ").concat(lastName);
+        return firstName + " " + lastName;
+//        return firstName.concat(" ").concat(lastName);
     }
-
 }
